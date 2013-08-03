133b25da4b32932ffaae08d73abf9d320d3f97d6
Gurjeet Singh <singh.gurjeet@gmail.com>
2013-08-03 08:22:55 -0400

    Fix the JS code for CustomerManagementApp so it works locally everywhere.
    Similar to previous fix, replace absolute paths with relative ones.

--- app/app.js
+++ app/app.js
@@ -27,19 +27,19 @@ app.config(function ($routeProvider) {
         .when('/customers',
             {
                 controller: 'CustomersController',
-                templateUrl: '/app/partials/customers.html'
+                templateUrl: 'app/partials/customers.html'
             })
         //Define a route that has a route parameter in it (:customerID)
         .when('/customerorders/:customerID',
             {
                 controller: 'CustomerOrdersController',
-                templateUrl: '/app/partials/customerOrders.html'
+                templateUrl: 'app/partials/customerOrders.html'
             })
         //Define a route that has a route parameter in it (:customerID)
         .when('/orders',
             {
                 controller: 'OrdersController',
-                templateUrl: '/app/partials/orders.html'
+                templateUrl: 'app/partials/orders.html'
             })
         .otherwise({ redirectTo: '/customers' });
 });
